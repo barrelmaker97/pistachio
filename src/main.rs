2103d15d3789c307b18e596fc00e376d97c7af76
Nolan Cooper <nolancooper97@gmail.com>
2024-08-09 18:04:12 -0500

    Poll UPS using rups

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, thread, time};
+
+use rups::blocking::Connection;
+use rups::{Auth, ConfigBuilder};
+use std::convert::TryInto;
+
+fn main() -> rups::Result<()> {
+    let host = env::var("UPS_HOST").unwrap_or_else(|_| "localhost".into());
+    let port = env::var("UPS_PORT")
+        .ok()
+        .map(|s| s.parse::<u16>().ok())
+        .flatten()
+        .unwrap_or(3493);
+
+    let username = env::var("UPS_USER").ok();
+    let password = env::var("UPS_PASSWORD").ok();
+    let auth = username.map(|username| Auth::new(username, password));
+
+    let config = ConfigBuilder::new()
+        .with_host((host, port).try_into().unwrap_or_default())
+        .with_auth(auth)
+        .with_debug(false) // Turn this on for debugging network chatter
+        .build();
+
+    let mut conn = Connection::new(&config)?;
+
+    let poll_rate = 5;
+
+    // Print a list of all UPS devices
+    let mut counter = 0;
+    loop {
+        if counter % poll_rate == 0 {
+            println!("Connected UPS devices:");
+            for (name, description) in conn.list_ups()? {
+                println!("\t- Name: {}", name);
+                println!("\t  Description: {}", description);
+
+                // List UPS variables (key = val)
+                println!("\t  Variables:");
+                for var in conn.list_vars(&name)? {
+                    println!("\t\t- {}", var);
+                }
+            }
+        }
+        counter += 1;
+        thread::sleep(time::Duration::from_secs(1));
+    }
 }
