c96166d727f1e1da343a40533786218ecbbcf136
Nolan Cooper <nolancooper97@gmail.com>
2024-08-11 15:04:23 -0500

    Organize

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,13 @@ use env_logger::{Builder, Env};
 use log::info;
 
 fn main() -> rups::Result<()> {
-    // Setup exporter address
-    let addr_raw = "0.0.0.0:9184";
-    let addr: SocketAddr = addr_raw.parse().expect("Cannot parse listen address");
-
-    // Create metric and start exporter
-    let metric = register_gauge!("test_gauge", "test description").expect("Cannot create gauge");
-    metric.set(42.0);
-    prometheus_exporter::start(addr).expect("Cannot start exporter");
-
+    // Initialize logging
     Builder::from_env(Env::default().default_filter_or("info")).init();
     info!("Exporter started!");
 
+    // Set configuration and exporter address
+    let addr_raw = "0.0.0.0:9184";
+    let addr: SocketAddr = addr_raw.parse().expect("Cannot parse listen address");
     let poll_rate = 10;
     let host = env::var("UPS_HOST").unwrap_or_else(|_| "localhost".into());
     let port = env::var("UPS_PORT")
@@ -30,13 +25,20 @@ fn main() -> rups::Result<()> {
         .map(|s| s.parse::<u16>().ok())
         .flatten()
         .unwrap_or(3493);
-
     let config = ConfigBuilder::new()
         .with_host((host, port).try_into().unwrap_or_default())
         .with_debug(false) // Turn this on for debugging network chatter
         .build();
 
+    // Create connection to UPS
     let mut conn = Connection::new(&config)?;
+
+    // Create metric and start exporter
+    let metric = register_gauge!("test_gauge", "test description").expect("Cannot create gauge");
+    prometheus_exporter::start(addr).expect("Cannot start exporter");
+
+    // TEST METRIC
+    metric.set(42.0);
 
     // Print a list of all UPS devices
     let mut counter = 0;
