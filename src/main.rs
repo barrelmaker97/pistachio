2a141393b067937876c61854d817ad8be6b23e22
Nolan Cooper <nolancooper97@gmail.com>
2024-08-14 21:09:36 -0500

    Set float metrics

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use prometheus_exporter::prometheus::register_gauge;
 use prometheus_exporter::prometheus::register_gauge_vec;
 
 use env_logger::{Builder, Env};
-use log::info;
+use log::{debug, info};
 
 fn main() -> rups::Result<()> {
     // Initialize logging
@@ -21,8 +21,8 @@ fn main() -> rups::Result<()> {
     let addr_raw = "0.0.0.0:9120";
     let addr: SocketAddr = addr_raw.parse().expect("Cannot parse listen address");
     let ups_name = env::var("UPS_NAME").unwrap_or_else(|_| "ups".into());
-    let host = env::var("UPS_HOST").unwrap_or_else(|_| "localhost".into());
-    let port = env::var("UPS_PORT")
+    let ups_host = env::var("UPS_HOST").unwrap_or_else(|_| "localhost".into());
+    let ups_port = env::var("UPS_PORT")
         .ok()
         .map(|s| s.parse::<u16>().ok())
         .flatten()
@@ -34,12 +34,12 @@ fn main() -> rups::Result<()> {
         .unwrap_or(10);
 
     // Log config info
-    info!("UPS to be checked: {ups_name}@{host}:{port}");
+    info!("UPS to be checked: {ups_name}@{ups_host}:{ups_port}");
     info!("Poll Rate: Every {poll_rate} seconds");
 
     // Create connection to UPS
     let config = ConfigBuilder::new()
-        .with_host((host, port).try_into().unwrap_or_default())
+        .with_host((ups_host, ups_port).try_into().unwrap_or_default())
         .with_debug(false) // Turn this on for debugging network chatter
         .build();
     let mut conn = Connection::new(&config)?;
@@ -48,13 +48,15 @@ fn main() -> rups::Result<()> {
     let mut metrics = HashMap::new();
     let available_vars = conn.list_vars(&ups_name).expect("Failed to connect to the UPS");
     for var in available_vars {
-        let var_desc = conn.get_var_description(&ups_name, &var.name()).expect("Failed to get variable description");
-        let mut var_name = var.name().replace(".", "_");
-        if !var_name.starts_with("ups") {
-            var_name.insert_str(0, "ups_");
+        if let Ok(_) = var.value().parse::<f64>() {
+            let var_desc = conn.get_var_description(&ups_name, &var.name()).expect("Failed to get variable description");
+            let mut var_name = var.name().replace(".", "_");
+            if !var_name.starts_with("ups") {
+                var_name.insert_str(0, "ups_");
+            }
+            let gauge = register_gauge!(var_name, var_desc).expect("Could not create gauge");
+            metrics.insert(String::from(var.name()), gauge);
         }
-        let gauge = register_gauge!(var_name, var_desc).expect("Could not create gauge");
-        metrics.insert(String::from(var.name()), gauge);
     }
     info!("{} metrics available to be exported", metrics.len());
 
@@ -73,18 +75,18 @@ fn main() -> rups::Result<()> {
     // Print a list of all UPS devices
     let mut counter = 0;
     loop {
+        debug!("Loop counter {counter}");
         if counter % poll_rate == 0 {
             // List UPS variables (key = val)
+            debug!("Polling UPS...");
             for var in conn.list_vars(&ups_name)? {
-                println!("{}", var);
-                // There are problems right now since label gauges are not being made
-                // They are being parsed as though they are simple gauges
-                //match metrics.get(var.name().into()) {
-                    //Some(gauge) => gauge.set(var.value().parse().unwrap()),
-                    //None => info!("Failed to update a gauge")
-                //}
+                if let Ok(_) = var.value().parse::<f64>() {
+                    match metrics.get(var.name().into()) {
+                        Some(gauge) => gauge.set(var.value().parse().unwrap()),
+                        None => info!("Failed to update a gauge")
+                    }
+                }
             }
-            println!("\n");
         }
         counter += 1;
         thread::sleep(time::Duration::from_secs(1));
